87bccf30ef8b4c78502b7c52d179988763eb8a6e
NatLey30 <112613481+NatLey30@users.noreply.github.com>
2022-11-24 09:29:48 +0100

    .

--- generar_excel.py
+++ generar_excel.py
@@ -3,73 +3,57 @@ from openpyxl.drawing.image import Image
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import xlsxwriter
 
 if "__main__" == __name__:
 
+    # Abrimos dataframes
     comprar = pd.read_csv("compra_semanal.csv", sep =',')
     orders = pd.read_csv("order_details_ordenado.csv", sep =',')
+    pizzas = pd.read_csv('pizzas.csv')
 
-    libro = openpyxl.Workbook()
-    libro.remove(libro.active)
+    # Los ordenamos
+    pizzas = pizzas.sort_values('price', ascending=False)
 
-    hoja1 = libro.create_sheet("Reporte ejecutivo")
-
-    df1 = pd.read_csv('compra_semanal.csv')
-    df2 = pd.read_csv('order_details_ordenado.csv')
-    df3 = pd.read_csv('pizzas.csv')
-
-    # Creamos gráficos
-    ## Ingredientes menos usados
-    pequeños = df1.nsmallest(5, 'Porciones')
-    plt.figure(figsize=(10, 5))
-    ax = sns.barplot(x='Ingredientes', y='Porciones', data=pequeños, palette='rocket_r')
-    plt.savefig('Ingredientes_menos_usados.jpg')
-
-    ## Ingredientas más usados
-    mayores = df1.nlargest(5, 'Porciones')
-    plt.figure(figsize=(10, 5))
-    ax = sns.barplot(x='Ingredientes', y='Porciones', data=mayores, palette='rocket_r')
-    plt.savefig('Ingredientes_mas_usados.jpg')
-
-    ## Pizzas más pedidas
-    plt.figure(figsize=(10, 5))
-    ax = sns.countplot(data=df2, x='pizza_id', order=pd.value_counts(df2['pizza_id']).iloc[:5].index)
-    ax.set(xlabel='pizza_id', ylabel='Cantidad')
-    plt.savefig('Pizzas_mas_pedidas.jpg')
-
-    ## Pizzas más caras
-    data = df3.sort_values('price', ascending=False).head(6)
-    plt.figure(figsize=(10, 5))
-    ax = sns.barplot(x='pizza_id', y='price', data=data, palette='rocket_r')
-    ax.set(xlabel='pizza_id', ylabel='price')
-    plt.savefig('Pizzas_mas_caras.jpg')
-
-    ## Pizzas más baratas
-    data = df3.sort_values('price', ascending=False).tail(6)
-    plt.figure(figsize=(10, 5))
-    ax = sns.barplot(x='pizza_id', y='price', data=data, palette='rocket_r')
-    ax.set(xlabel='pizza_id', ylabel='price')
-    plt.savefig('Pizzas_mas_baratas.jpg')
-
-    imagen1 = Image('Ingredientes_menos_usados.jpg')
-    imagen2 = Image('Ingredientes_mas_usados.jpg')
-    imagen3 = Image('Pizzas_mas_baratas.jpg')
-    imagen4 = Image('Pizzas_mas_caras.jpg')
-    imagen5 = Image('Pizzas_mas_pedidas.jpg')
-
-    hoja1['A1'] = 'En la siguiente gráfica se muestran los 5 ingredientes menos usados'
-    hoja1.add_image(imagen1, 'B2')
-    hoja1['T1'] = 'En la siguiente gráfica se muestran los 5 ingredientes más usados'
-    hoja1.add_image(imagen2, 'U2')
-    hoja1['A31'] = 'En la siguiente gráfica se muestran las 5 pizzas más baratas'
-    hoja1.add_image(imagen3, 'B32')
-    hoja1['T31'] = 'En la siguiente gráfica se muestran las 5 pizzas más caras'
-    hoja1.add_image(imagen4, 'U32')
-    hoja1['A61'] = 'En la siguiente gráfica se muestran las 5 pizzas más vendidas'
-    hoja1.add_image(imagen5, 'B62')
-
-    libro.save('reporte.xlsx')
-
-    with pd.ExcelWriter("reporte.xlsx", mode="a", engine="openpyxl") as writer:
+    with pd.ExcelWriter("Informe_Pizzerias_Maven.xlsx", mode="w", engine="xlsxwriter") as writer:
+        pizzas.to_excel(writer, sheet_name='Reporte ejecutivo', index=False)
         comprar.to_excel(writer, sheet_name='Reporte ingredientes', index=False)
         orders.to_excel(writer, sheet_name='Reporte pedidos', index=False)
+
+        wb  = writer.book
+
+        # Gráficos primera hoja
+        hoja1 = 'Reporte ejecutivo'
+
+        chart1 = wb.add_chart({'type': 'bar'})
+        chart1.add_series({'categories': f'{hoja1}!$B$2:$B$6', 'values': f'{hoja1}!$D$2:$D$6'})
+
+        chart1.set_title ({'name': 'Pizzas más caras'})
+        chart1.set_x_axis({'name': 'Euros'})
+        chart1.set_y_axis({'name': 'Pizzas'})
+        chart1.set_style(11)
+
+        writer.sheets['Reporte ejecutivo'].insert_chart('F2', chart1, {'x_offset': 25, 'y_offset': 10})
+
+        chart2 = wb.add_chart({'type': 'bar'})
+        chart2.add_series({'categories': f'{hoja1}!$B$93:$B$97', 'values': f'{hoja1}!$D$93:$D$97'})
+
+        chart2.set_title ({'name': 'Pizzas más baratas'})
+        chart2.set_x_axis({'name': 'Euros'})
+        chart2.set_y_axis({'name': 'Pizzas'})
+        chart2.set_style(11)
+
+        writer.sheets['Reporte ejecutivo'].insert_chart('F22', chart2, {'x_offset': 25, 'y_offset': 10})
+
+        # Gráficos segunda hoja
+        hoja2 = 'Reporte ingredientes'
+
+        chart3 = wb.add_chart({'type': 'bar'})
+        chart3.add_series({'categories': f'{hoja2}!$A$2:$A$66', 'values': f'{hoja2}!$B$2:$B$66'})
+
+        chart3.set_title ({'name': 'Ingredientes'})
+        chart3.set_x_axis({'name': 'Porciones'})
+        chart3.set_y_axis({'name': 'Ingredientes'})
+        chart3.set_style(11)
+
+        writer.sheets['Reporte ingredientes'].insert_chart('F2', chart3, {'x_offset': 50, 'y_offset': 20})
